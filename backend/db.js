22dea4ffce08c40d71e4f67ab665a68c96237f5c
diegoa1999 <74573232+diegoa1999@users.noreply.github.com>
2026-01-15 15:38:20 -0500

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -4,9 +4,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-
-  // 👇 ESTO ES LO CLAVE EN CLOUD RUN + CLOUD SQL
-  socketPath: process.env.DB_SOCKET, 
+  socketPath: process.env.DB_SOCKET,
 });
 
 export default pool;
